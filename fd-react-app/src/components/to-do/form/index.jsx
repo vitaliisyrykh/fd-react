441d7e016389443457df77a1a5e9ad296ba210d9
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-03-09 23:24:29 +0200

    Add errorForToDo

--- fd-react-app/src/components/to-do/form/index.jsx
+++ fd-react-app/src/components/to-do/form/index.jsx
@@ -1,5 +1,5 @@
-import React,{ useState} from 'react';
-import {Formik,Form, Field} from 'formik';
+import React from 'react';
+import {Formik,Form, Field, ErrorMessage} from 'formik';
 
 const FormTodo = props => {
   const {onSubmit} = props;
@@ -16,6 +16,7 @@ const FormTodo = props => {
       <Formik  initialValues ={initialValues} onSubmit = {onSubmit}  >
         <Form>
           <Field name="todoBody" type="text" placeholder="enter text" />
+          <ErrorMessage name ="todoBody" component="div"/>
           <Field name="submit" value="Add task" type="submit"/>
         </Form>
       </Formik>
